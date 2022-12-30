5c28bd46d8dd65a31fa2f6bacd56f7bcb138aef3
Jeong Jin Hui <65870263+rhaehf@users.noreply.github.com>
2022-12-30 13:17:34 +0900

    [Bronze I] Title: 이항 계수 1, Time: 124 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        System.out.println(factorial(N) / (factorial(K) * factorial(N - K)));
+    }
+
+    static int factorial(int N) {
+        if (N == 0) {
+            return 1;
+        }
+
+        return N * factorial(N - 1);
+    }
+}
