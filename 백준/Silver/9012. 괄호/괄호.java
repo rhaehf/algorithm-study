b1e1b0c8059ba323c4a88b72139ca1619e27595e
Jeong Jin Hui <65870263+rhaehf@users.noreply.github.com>
2023-01-29 01:23:39 +0900

    [Silver IV] Title: 괄호, Time: 132 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,19 +1,18 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
-        String result = "";
 
         for (int i = 0; i < T; i++) {
-            char input[] = br.readLine().toCharArray();
-//            System.out.println(Arrays.toString(input));
-
+            String result = "";
             Stack<Character> myStack = new Stack<Character>();
+
+            char input[] = br.readLine().toCharArray();
+            
             for (int j = 0; j < input.length; j++) {
                 char c = input[j];
 
@@ -22,22 +21,18 @@ public class Main {
                 }
                 else if (myStack.isEmpty()) {
                     result = "NO";
-//                    System.out.println("NO !!!");
                     break;
                 }
                 else {
                     myStack.pop();
                 }
-//                System.out.println(myStack);
             }
 
-//            System.out.println(myStack);
             if (!result.equals("NO") && myStack.isEmpty()) {
                 System.out.println("YES");
             } else {
                 System.out.println("NO");
             }
-            result = "";
         }
     }
 }
