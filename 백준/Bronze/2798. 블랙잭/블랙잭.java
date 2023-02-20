0f552685a3472ca38a3325865a60384353be9d3a
Jeong Jin Hui <65870263+rhaehf@users.noreply.github.com>
2023-02-21 01:12:53 +0900

    [Bronze II] Title: 블랙잭, Time: 128 ms, Memory: 14380 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,11 +1,10 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
@@ -13,27 +12,20 @@ public class Main {
         int M = Integer.parseInt(st.nextToken());
 
         String[] cardList = br.readLine().split(" ");
-
-//        ArrayList<Integer> numList = new ArrayList<>();
-//        for(String num: cardList) {
-//            numList.add(Integer.parseInt(num));
-//        }
-//        System.out.println(numList);
-
         int[] cards = Arrays.stream(cardList).mapToInt(Integer::parseInt).toArray();
 //        System.out.println(Arrays.toString(cards));
 
         int max = 0;
+
         // 브루트포스 알고리즘 사용
         // 3개의 카드를 고르기 때문에 첫번째 카드 i는 N-2까지 순회
-        for (int i = 0; i < N-2; i++) {
+        for (int i = 0; i < N - 2; i++) {
             // 두번째 카드 j는 i 다음부터 N-1까지 순회
-            for (int j = i+1; j < N-1; j++) {
+            for (int j = i + 1; j < N - 1; j++) {
                 // 세번재 카드 k는 j 다음부터 N까지 순회
-                for (int k = j+1; k < N; k++) {
-//                    int sum = numList.get(i) + numList.get(j) + numList.get(k);
+                for (int k = j + 1; k < N; k++) {
                     int sum = cards[i] + cards[j] + cards[k];
-                    if(sum <= M){
+                    if (sum <= M) {
                         max = Math.max(max, sum);
                     }
                 }
