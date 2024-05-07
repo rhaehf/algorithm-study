4a8e2de025ebb6e04ac0c16a2e914b5a3685e1a6
Jeong Jin Hui <65870263+rhaehf@users.noreply.github.com>
2024-05-07 23:41:59 +0900

    [Silver V] Title: 덩치, Time: 232 ms, Memory: 18732 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.java
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -1,11 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class Main {
     static int[] weights;
@@ -49,7 +47,7 @@ public class Main {
             result[one] = rate + 1;
         }
 
-        System.out.println(IntStream.of(result).mapToObj(Integer::toString).collect(Collectors.joining(" ")));
-//        System.out.println(IntStream.of(result).boxed().map(Object::toString).collect(Collectors.joining(", ")));
+//        System.out.println(IntStream.of(result).mapToObj(Integer::toString).collect(Collectors.joining(" ")));
+        System.out.println(IntStream.of(result).boxed().map(Object::toString).collect(Collectors.joining(" ")));
     }
 }
