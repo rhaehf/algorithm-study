c7f69f4259721d057e1a9f7be6099a0e70cadb10
Jeong Jin Hui <jda0526@naver.com>
2024-06-27 14:56:44 +0900

    [Bronze II] Title: 문자열 반복, Time: 100 ms, Memory: 14108 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.java
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -1,33 +1,25 @@
-//package P2675;
-
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
 
         StringTokenizer st;
-        ArrayList<String> sentence = new ArrayList<>();
-
         for (int i = 0; i < T; i++) {
             st = new StringTokenizer(br.readLine());
-            int repeat = Integer.parseInt(st.nextToken());
-            String words = st.nextToken();
+            int repeatNum = Integer.parseInt(st.nextToken());
+            String word = st.nextToken();
 
             StringBuilder sb = new StringBuilder();
-            for (int j = 0; j < words.length(); j++) {
-                char at = words.charAt(j);
-                for (int k = 0; k < repeat; k++) {
-                    sb.append(at);
-                }
+            for (int j = 0; j < word.length(); j++) {
+                char at = word.charAt(j);
+                sb.append(String.valueOf(at).repeat(repeatNum));
             }
-            sentence.add(sb.toString());
+            System.out.println(sb);
         }
-        String result = String.join( "\n", sentence);
-        System.out.println(result);
     }
 }
