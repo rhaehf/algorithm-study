842a0dce6e3861dd609c648bb4cb811985158b3d
Jeong Jin Hui <jda0526@naver.com>
2024-06-28 12:30:45 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 68 ms, Memory: 11504 KB -BaekjoonHub

---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String word = br.readLine();
+        int result = 1;
+        for (int i = 0; i < word.length() / 2; i++) {
+            if (word.charAt(i) != word.charAt(word.length() - 1 - i)) {
+                result = 0;
+            }
+        }
+        System.out.println(result);
+    }
+}
