81ff0263ac239a943ad9a80281bceb1ab5010e72
Jeong Jin Hui <jda0526@naver.com>
2024-12-18 23:42:25 +0900

    [Silver III] Title: 바이러스, Time: 104 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -8,41 +8,45 @@ import java.util.StringTokenizer;
 public class Main {
     static int[][] graph; // 그래프 배열
     static boolean[] visit; // 방문 체크 배열
-    static int virusCnt = 0; // 바이러스에 걸린 컴퓨터 수
+    static int virus = 0; // 바이러스에 걸린 컴퓨터 수
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int computerCnt = Integer.parseInt(br.readLine()); // 컴퓨터 수
-        int lineCnt = Integer.parseInt(br.readLine()); // 연결된 컴퓨터 수
+        int N = Integer.parseInt(br.readLine()); // 컴퓨터 수
+        int connection = Integer.parseInt(br.readLine()); // 연결된 컴퓨터 수
 
-        graph = new int[computerCnt + 1][computerCnt + 1];
-        visit = new boolean[computerCnt + 1];
+        // 인덱스 1번부터 사용
+        graph = new int[N + 1][N + 1];
+        visit = new boolean[N + 1];
 
-        for (int i = 0; i < lineCnt; i++) {
+        for (int i = 0; i < connection; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
+            // 양방향 그래프인 문제에서는 꼭 두 방향의 연결을 모두 표현해야함
             graph[a][b] = 1;
-            graph[b][a] = 1;
+            graph[b][a] = 1; // 양방향 연결을 표현하기 위해 반대로도 표시해줌
         }
 
-        dfs(1);
-        System.out.println(virusCnt - 1);
+        bfs(1);
     }
 
-    static void dfs(int computer) { // 깊이 우선 탐색 구현
-        visit[computer] = true; // 1. 방문 처리
-        virusCnt++;
-//        System.out.print("computer : ");
-//        System.out.println(computer);
+    public static void bfs(int start) { // 너비 우선 탐색 구현
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start); // 1. 큐에 시작 노드 add
+        visit[start] = true; // 2. 방문 처리
 
-        for (int i = 0; i < graph.length; i++) {
-            if (graph[computer][i] == 1 && visit[i] == false) {
-//                System.out.print("인접해있고, 방문하지않은 노드 i : ");
-//                System.out.println(i);
-                dfs(i); // 2. 내부적으로 재귀 호출
+        while (!queue.isEmpty()) { // 3-1. 큐가 비어있는지 확인 (비어있다면 탐색 종료)
+            int computer = queue.poll(); // 3-2. 큐에서 노드를 poll
+            for (int i = 0; i < graph.length; i++) { // poll한 노드와 인접한 노드 찾기
+                if (graph[computer][i] == 1 && visit[i] == false) { // 3-3. 인접해있고 && 방문하지 않았다면
+                    queue.add(i); // 3-4. 아직 방문하지 않은 노드를 큐에 add 하고
+                    visit[i] = true; // 3-5. 방문 처리
+                    virus++; // 인접해있기에 바이러스에 감염됨
+                }
             }
         }
+        System.out.println(virus);
     }
 }
