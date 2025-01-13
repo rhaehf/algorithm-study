70d3e408d999ebe57b7fa55340eb5c3dc3d0d387
Jeong Jin Hui <jda0526@naver.com>
2025-01-14 01:31:45 +0900

    [Silver II] Title: 유기농 배추, Time: 136 ms, Memory: 16204 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -37,8 +37,9 @@ public class Main {
             for (int j = 0; j < M; j++) { // 가로
                 for (int k = 0; k < N; k++) { // 세로
                     if (map[j][k] == 1 && !visit[j][k]) {
-                        // 배추가 있고, 방문 안한 좌표에서부터 bfs로 연결된 곳을 파악
-                        bfs(j, k);
+                        // 배추가 있고, 방문 안한 좌표에서부터 bfs, dfs로 연결된 곳을 파악
+//                        bfs(j, k);
+                        dfs(j, k);
 
                         // 지렁이의 개수는 인접한 곳마다 1개씩이다.
                         // 인접한 곳을 모두 파악했으면 지렁이 개수 1 증가
@@ -50,40 +51,26 @@ public class Main {
         }
     }
 
-    public static void bfs(int startX, int startY) {
-        // bfs가 1번 실행될때마다 인접한 곳을 모두 탐색하고 종료되니 bfs안에 queue를 선언했다.
-        Queue<int[]> queue = new LinkedList<>();
-        // x, y좌표 저장
-        queue.offer(new int[]{startX, startY});
-        // 시작 좌표엔 배추가 있으니 미리 true로 처리한다.
+    private static void dfs(int startX, int startY) {
+        // 방문 체크
         visit[startX][startY] = true;
 
-        // 배추가 상하좌우에 인접하면 이동할 수 있다.
-        // 현재 좌표에서 상하좌우 움직일 좌표 지정
         int[] X = {0, 0, -1, +1};
         int[] Y = {-1, +1, 0, 0};
 
-        //queue가 비어있으면 더이상 인접한 배추가 없다는 뜻이다.
-        while (!queue.isEmpty()) {
-            // 저장된 queue 꺼내기
-            int[] node = queue.poll();
-            // 상하좌우 확인
-            for (int i = 0; i < 4; i++) {
-                int nowX = node[0] + X[i];
-                int nowY = node[1] + Y[i];
+        // 상하좌우 확인
+        for (int i = 0; i < 4; i++) {
+            // 상하좌우로 탐색할 좌표 설정
+            int nowX = startX + X[i];
+            int nowY = startY + Y[i];
 
-                // 좌표가 배추밭을 벗어나게되면 다음 좌표를 체크
-                if (nowX < 0 || nowX >= M || nowY < 0 || nowY >= N) {
-                    continue;
-                }
-
-                // 상하좌우 움직인 좌표에 배추가 있고, 체크하지 않은 좌표이면
-                if (map[nowX][nowY] == 1 & !visit[nowX][nowY]) {
-                    // 좌표를 저장하고
-                    queue.offer(new int[]{nowX, nowY});
-                    // 방문 체크를 함
-                    visit[nowX][nowY] = true;
-                }
+            // 좌표가 배추밭을 벗어나게되면 다음 좌표를 체크
+            if (nowX < 0 || nowX >= M || nowY < 0 || nowY >= N) {
+                continue;
+            }
+            // 상하좌우 움직인 좌표에 배추가 있고, 체크하지 않은 좌표이면
+            if (map[nowX][nowY] == 1 & !visit[nowX][nowY]) {
+                dfs(nowX, nowY); // 해당 좌표로 dfs 재귀 실행
             }
         }
     }
