9d7cfbacd0b2c6e07669b1e9400d4e78d1f866f9
Jeong Jin Hui <jda0526@naver.com>
2025-01-16 03:31:21 +0900

    [Silver III] Title: 블로그, Time: 312 ms, Memory: 35424 KB -BaekjoonHub

--- 백준/Silver/21921. 블로그/블로그.java
+++ 백준/Silver/21921. 블로그/블로그.java
@@ -10,37 +10,39 @@ public class Main {
         int N = Integer.parseInt(st.nextToken()); // 블로그 시작한 일수
         int X = Integer.parseInt(st.nextToken()); // 몇 일동안의 방문자 수를 구할건지 기간
 
-        int[] visits = new int[N];
+        int[] days = new int[N];
         st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < N; i++) {
-            visits[i] = Integer.parseInt(st.nextToken());
+            days[i] = Integer.parseInt(st.nextToken());
         }
 
-        int visitSum = 0; // 방문자 수의 합
+        int visitCount = 0; // 방문자 수의 합
+        int maxVisit = 0; // 최대 방문자 합의 횟수
+        int count = 0; // 최대 방문자 수를 기록한 횟수
+
         // 처음 X일 동안 방문자 수의 합
         for (int i = 0; i < X; i++) {
-            visitSum += visits[i];
+            visitCount += days[i];
         }
+        maxVisit = visitCount;
 
-        int maxVisit = visitSum; // 최대 방문자 수
-        int count = 1; // 최대 방문자 수를 기록한 횟수
+        // 최대 방문자 수가 0명이 아닌 경우 최대 방문자 수 기록은 최소 1이 된다.
+        if (maxVisit != 0)
+            count = 1;
 
         // 슬라이딩 윈도우(투 포인터) 사용
-        for (int i = X; i < visits.length; i++) {
-            visitSum = visitSum - visits[i-X] + visits[i];
+        for (int i = X; i < days.length; i++) {
+            visitCount = visitCount - days[i - X] + days[i];
 
-            // 최대 방문자 수와 동일한 경우
-            if(visitSum == maxVisit)
+            if (visitCount == maxVisit)
                 count++;
-            
-            // 최대 방문자 수가 갱신된 경우
-            if(visitSum > maxVisit){
-                maxVisit = visitSum;
-                count = 1;
+            else if (visitCount > maxVisit) {
+                maxVisit = visitCount;
+                count = 1; // 새로운 최대 방문자 수가 갱신되면 초기화
             }
         }
 
-        if(maxVisit == 0)
+        if (maxVisit == 0)
             System.out.println("SAD");
         else {
             System.out.println(maxVisit);
