34ad40605ea2387a64717822601041d211fb9844
Jeong Jin Hui <jda0526@naver.com>
2025-02-14 20:25:22 +0900

    [level 0] Title: 간단한 논리 연산, Time: 0.03 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.java
+++ 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.java
@@ -1,5 +1,6 @@
 class Solution {
-    public boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {        
+    public boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {      
+        // 논리합 OR ∨ , 논리곱 AND ∧
         return (x1 || x2) && (x3 || x4);
     }
 }
