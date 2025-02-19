01899f1fada16ae7bede1eab510014073e397ae9
Jeong Jin Hui <jda0526@naver.com>
2025-02-19 21:30:16 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.04 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -2,7 +2,12 @@ import java.util.*;
 
 class Solution {
     public int solution(String my_string, String is_suffix) {
-             
+        // 두번째 방법
+        // endsWith() 메서드는 문자열이 특정 접미사(suffix)로 끝나는지 여부를 확인하는 메서드이다.
+        return my_string.endsWith(is_suffix) ? 1 : 0;
+        
+        // 첫번째 방법
+        /*
         List<String> suffixList = new ArrayList<>();
 
         for (int i = 0; i < my_string.length(); i++) {
@@ -10,5 +15,6 @@ class Solution {
         }
 
         return suffixList.contains(is_suffix) ? 1 : 0;
+        */
     }
 }
