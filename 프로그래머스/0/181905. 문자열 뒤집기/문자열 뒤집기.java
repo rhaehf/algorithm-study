337dbd3b0b5e1edad2a30e9a72e9d425728989e1
Jeong Jin Hui <jda0526@naver.com>
2025-02-22 22:32:44 +0900

    [level 0] Title: 문자열 뒤집기, Time: 28.62 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -2,11 +2,18 @@ class Solution {
     public String solution(String my_string, int s, int e) {
         String answer = my_string.substring(0,s);
         
+        // 두번째 풀이 - StringBuffer 클래스와 StringBuilder 클래스의 reverse( ) 메서드 사용
+        StringBuilder tmp = new StringBuilder(my_string.substring(s, e + 1));        
+        return answer + tmp.reverse() + my_string.substring(e+1);
+        
+        // 첫번째 풀이
+        /*
         char[] array = my_string.substring(s,e+1).toCharArray();
         for(int i = array.length - 1; 0 <= i ; i--){
             answer += array[i];
         }
         
         return answer + my_string.substring(e+1);
+        */
     }
 }
