00e1c631ca869322b1a93f27535476b6cdcb1d88
Jeong Jin Hui <jda0526@naver.com>
2025-02-22 22:53:27 +0900

    [level 0] Title: qr code, Time: 0.12 ms, Memory: 92.4 MB -BaekjoonHub

--- 프로그래머스/0/181903. qr code/qr code.java
+++ 프로그래머스/0/181903. qr code/qr code.java
@@ -1,5 +1,15 @@
 class Solution {
     public String solution(int q, int r, String code) {
+        // 두번째 방법 - r은 나머지, i에 몫만큼 더하기
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = r; i < code.length(); i += q)
+            sb.append(code.charAt(i));
+
+        return sb.toString();        
+        
+        // 첫번째 방법
+        /*
         String answer = "";
         
         for(int i = 0; i < code.length(); i++){
@@ -8,5 +18,6 @@ class Solution {
         }        
         
         return answer;
+        */
     }
 }
