6437c6dbdac80b47db6a18bc26ca98098c1bea1d
Jeong Jin Hui <jda0526@naver.com>
2025-02-27 20:46:39 +0900

    [level 0] Title: 배열 만들기 1, Time: 0.11 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
@@ -2,6 +2,19 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n, int k) {
+        // 두번째 방법
+        int count = n / k;
+
+        int[] answer = new int[count];
+
+        for (int i = 1; i <= count; i++) {
+            answer[i - 1] = k * i;
+        }
+
+        return answer;
+        
+        // 첫번째 방법
+        /*
         List<Integer> answer = new ArrayList<>();
         
         for(int i = k; i <= n; i += k){
@@ -11,5 +24,6 @@ class Solution {
         return answer.stream()
                 .mapToInt(i -> i)
                 .toArray();
+        */
     }
 }
