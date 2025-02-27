0563275a1553820cdb6c2f936d4e9674c3bbf528
Jeong Jin Hui <jda0526@naver.com>
2025-02-27 21:27:13 +0900

    [level 0] Title: 카운트 다운, Time: 0.03 ms, Memory: 89.2 MB -BaekjoonHub

--- 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
@@ -2,9 +2,9 @@ class Solution {
     public int[] solution(int start_num, int end_num) {
         int[] answer = new int[start_num - end_num + 1];
         
-        int num = start_num;
         for(int i = 0; i <= start_num - end_num; i++)
-            answer[i] = num--;
+            answer[i] = start_num - i;
+
         return answer;
     }
 }
