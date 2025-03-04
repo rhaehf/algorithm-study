2c112d83b2896a8933aa54354d0416e7dae6726b
Jeong Jin Hui <jda0526@naver.com>
2025-03-04 10:52:55 +0900

    [Bronze V] Title: 새싹, Time: 60 ms, Memory: 11416 KB -BaekjoonHub

--- 백준/Bronze/25083. 새싹/새싹.java
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -2,10 +2,10 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.println("         ,r'\"7");
+        System.out.println("         ,r'\"7"); // \" 따옴표
         System.out.println("r`-_   ,'  ,/");
-        System.out.println(" \\. \". L_r'");
-        System.out.println("   `~\\/");
+        System.out.println(" \\. \". L_r'"); // \\ 백슬래시, \" 따옴표
+        System.out.println("   `~\\/"); // \\ 백슬래시
         System.out.println("      |");
         System.out.println("      |");
     }
