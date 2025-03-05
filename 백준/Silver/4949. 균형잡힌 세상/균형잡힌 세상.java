1046bb8e4e5d64563f2c1e002f093f4baeee11a6
Jeong Jin Hui <jda0526@naver.com>
2025-03-05 22:05:13 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 196 ms, Memory: 18996 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        while (true) {
+            String line = br.readLine();
+            // 입력 종료 조건으로 맨 마지막에 "."이 들어옴
+            if (line.equals(".")) break;
+
+            Stack<Character> stack = new Stack<>();
+            for (char ch : line.toCharArray()) {
+                if (ch == '.') { // 문자열은 온점(".")으로 끝난다.
+                    break;
+                }
+
+                if (ch == '(' || ch == '[')
+                    stack.push(ch);
+                else if (ch == ')') {
+                    if (!stack.isEmpty() && stack.peek() == '(')
+                        stack.pop(); // 괄호가 짝을 이룰 때
+                    else if (stack.isEmpty()) { // 비어있는 있고, 괄호가 짝을 이루지 않을 때
+                        stack.push(ch);
+                        break;
+                    } else // 괄호가 짝을 이루지 않을 때
+                        break;
+                } else if (ch == ']') {
+                    if (!stack.isEmpty() && stack.peek() == '[')
+                        stack.pop();
+                    else if (stack.isEmpty()) { // 비어있는 있고, 괄호가 짝을 이루지 않을 때
+                        stack.push(ch);
+                        break;
+                    } else // 괄호가 짝을 이루지 않을 때
+                        break;
+                }
+            }
+
+            // 스택이 비어 있고, 모든 괄호가 올바르게 짝을 이루었을 때 "yes", 그렇지 않으면 "no"
+            sb.append(stack.isEmpty() ? "yes" : "no").append("\n");
+        }
+        System.out.println(sb);
+    }
+}
