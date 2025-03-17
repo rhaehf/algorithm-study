aa9bd227cd8696397f773683a258e527af05dd63
Jeong Jin Hui <jda0526@naver.com>
2025-03-17 21:56:06 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 968 ms, Memory: 173620 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,73 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[] arrayN;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        arrayN = new int[N];
 
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < N; i++) {
-            arrayN[i] = Integer.parseInt(st.nextToken());
+            int key = Integer.parseInt(st.nextToken());
+
+            /*
+             * getOrDefault(key, defaultValue)
+             * key에 대해 map에 저장 된 value를 반환한다.
+             * 만약 value가 없을 경우 defaultValue값을 반환한다.
+             */
+            // getOrDefault를 사용해 map에서 key에 대한 value를 확인해서
+            // key가 없으면 0을, 있으면 반환된 value에 1을 더해서 다시 map에 put 해준다.
+            map.put(key, map.getOrDefault(key, 0) + 1);
         }
-        Arrays.sort(arrayN); // 이분 탐색을 사용하기 위해서 정렬하기
 
         int M = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine(), " ");
-
         StringBuilder sb = new StringBuilder();
+
         for (int i = 0; i < M; i++) {
-            int target = Integer.parseInt(st.nextToken()); // 찾아야 하는 수
-//            int count = binarySearch(target);
-            int count = upperBound(target) - lowerBound(target);
-            sb.append(count).append(" ");
+            int key = Integer.parseInt(st.nextToken());
+            sb.append(map.getOrDefault(key, 0)).append("\n");
         }
-        System.out.println(sb.toString().trim());
-    }
-
-    // lower_bound: target이 처음 등장하는 위치 찾기
-    public static int lowerBound(int target) {
-        int low = 0;
-        int high = arrayN.length;
-
-        // low와 high가 같아질 때 까지 반복
-        while (low < high) {
-            int mid = (low + high) / 2;
-            if (target <= arrayN[mid]) {
-                // 처음 등장하는 위치를 찾는 것은 같은 값이 있는 요소들의 가장 왼쪽을 찾아내야하기 때문에
-                // 왼쪽으로 범위를 좁히기 위해선 high 값을 mid로 사용함으로써 범위를 줄인다.(= 상한선을 내린다.)
-                high = mid;
-            } else {
-                // 반대로 오른쪽으로 범위를 좁혀나가야 하기 때문에
-                // low 값을 mid 보다 크게해서 하한선을 올린다.
-                low = mid + 1;
-            }
-        }
-        return low;
-    }
-
-    // upper_bound: target을 초과하는 첫 번째 위치 찾기
-    public static int upperBound(int target) {
-        int low = 0;
-        int high = arrayN.length;
-
-        // low와 high가 같아질 때 까지 반복
-        while (low < high) {
-            int mid = (low + high) / 2;
-            if (target < arrayN[mid]) {
-                // 상한선을 내린다.
-                high = mid;
-            } else {
-                // 하한선을 올린다.
-                low = mid + 1;
-            }
-        }
-        return low;
+        System.out.println(sb);
     }
 }
