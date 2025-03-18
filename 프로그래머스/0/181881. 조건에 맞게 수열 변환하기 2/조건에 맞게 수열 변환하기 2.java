ae9d31d52f66cfac6fe8274dfc7b2346cb933102
Jeong Jin Hui <jda0526@naver.com>
2025-03-19 02:49:46 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 69.05 ms, Memory: 165 MB -BaekjoonHub

---
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] arr) {
+        int[] copy = new int[arr.length];
+
+        int count = -1;
+        while (true) {
+            //System.out.println("1번 arr (이번 턴에 사용됨) : " + Arrays.toString(arr));
+            copy = Arrays.copyOf(arr, arr.length);
+            count++;
+            //System.out.println("1번 copy (1번 arr를 1번 copy로 복사 해줌) : " + Arrays.toString(copy));
+
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] >= 50 && arr[i] % 2 == 0) {
+                    arr[i] = arr[i] / 2;
+                } else if (arr[i] < 50 && arr[i] % 2 == 1) {
+                    arr[i] = arr[i] * 2 + 1;
+                }
+            }
+
+            //System.out.println("2번 arr (계산 결과) : " + Arrays.toString(arr));
+            //System.out.println("2번 copy (2번 arr와 비교할 값) : " + Arrays.toString(copy));
+            //System.out.println("----------");
+
+            if (Arrays.equals(copy, arr))
+                break;
+        }
+
+        return count;
+    }
+}
