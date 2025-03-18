f81b51474edaa52971f3a75b3418365569babbf7
Jeong Jin Hui <jda0526@naver.com>
2025-03-19 03:17:26 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 6.41 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
@@ -3,7 +3,12 @@ class Solution {
         if(pat.length() > myString.length())
             return 0;
 
-        int answer = myString.toLowerCase().contains(pat.toLowerCase()) ? 1 : 0;
+        // 두번재 방법 : indexOf - 찾는 문자열이 없으면 -1을 반환, 있으면 문자열 앞에서부터 처음 발견되는 인덱스를 반환
+        int answer = myString.toLowerCase().indexOf(pat.toLowerCase()) != -1 ? 1 : 0;
+        
+        // 첫번째 방법
+        //int answer = myString.toLowerCase().contains(pat.toLowerCase()) ? 1 : 0;
+        
         return answer;
     }
 }
