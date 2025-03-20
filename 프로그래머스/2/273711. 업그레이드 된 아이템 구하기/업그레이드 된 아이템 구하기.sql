35ca1ada0fbd5f90f565f7395ed41738ba3bf432
Jeong Jin Hui <jda0526@naver.com>
2025-03-21 00:31:29 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -0,0 +1,10 @@
+SELECT ITEM_ID, ITEM_NAME, RARITY
+FROM ITEM_INFO 
+WHERE ITEM_ID IN (
+                  SELECT T.ITEM_ID
+                  FROM ITEM_INFO AS I
+                      JOIN ITEM_TREE AS T
+                      ON I.ITEM_ID = T.PARENT_ITEM_ID
+                  WHERE RARITY = 'RARE')
+ORDER BY 1 DESC                  
+;
