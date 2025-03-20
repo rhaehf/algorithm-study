1bc8d770c5d2284ea1f0f1eca111250fab4685b3
Jeong Jin Hui <jda0526@naver.com>
2025-03-21 00:07:09 +0900

    [level 2] Title: 특정 물고기를 잡은 총 수 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/298518. 특정 물고기를 잡은 총 수 구하기/특정 물고기를 잡은 총 수 구하기.sql
+++ 프로그래머스/2/298518. 특정 물고기를 잡은 총 수 구하기/특정 물고기를 잡은 총 수 구하기.sql
@@ -1,4 +1,7 @@
 SELECT COUNT(*) FISH_COUNT
 FROM FISH_INFO I JOIN FISH_NAME_INFO N USING (FISH_TYPE)
 WHERE FISH_NAME IN ('BASS', 'SNAPPER')
+-- WHERE FISH_TYPE IN (0, 1); 이 안되는 이유 : 
+-- 문제에서 제공된 예제일 뿐, 일반적인 문제 조건에서는 FISH_NAME_INFO 테이블에서 이 값을 확인해야 함.
+-- 하드코딩된 FISH_TYPE 값(0, 1)만 사용할 경우 유연성이 부족함.
 ;
