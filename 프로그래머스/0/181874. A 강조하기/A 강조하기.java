e859ba3c8589c240be74a2e59e63399b31b0d122
Jeong Jin Hui <jda0526@naver.com>
2025-03-21 01:08:51 +0900

    [level 0] Title: A 강조하기, Time: 0.08 ms, Memory: 71.2 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.java
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -1,5 +1,10 @@
 class Solution {
     public String solution(String myString) {
+        // 두번째 방법
+        return myString.toLowerCase().replaceAll("a", "A");
+        
+        /*
+        // 첫번째 방법
         String answer = "";
         
         for (char ch : myString.toLowerCase().toCharArray()) {
@@ -9,5 +14,6 @@ class Solution {
                 answer += ch;
         }
         return answer;
+        */
     }
 }
