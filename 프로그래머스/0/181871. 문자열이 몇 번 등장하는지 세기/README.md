34ca36b622985fe5e9881ad612378485d9a85d68
Jeong Jin Hui <jda0526@naver.com>
2025-03-22 02:56:33 +0900

    [level 0] Title: 문자열이 몇 번 등장하는지 세기, Time: 0.40 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/README.md
+++ 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열이 몇 번 등장하는지 세기 - 181871 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181871) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181871#) 
 
 ### 성능 요약
 
-메모리: 71.6 MB, 시간: 0.15 ms
+메모리: 81.8 MB, 시간: 0.40 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 22일 02:52:02
+2025년 03월 22일 02:56:32
 
 ### 문제 설명
 
--- 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.java
+++ 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.java
@@ -1,5 +1,16 @@
 class Solution {
     public int solution(String myString, String pat) {
+        // 두번째 방법 : startsWith - 입력된 문자열로 시작하는지 확인하고 true/false 반환
+        int count = 0;
+        for (int i = 0; i < myString.length(); i++) {
+            if (myString.substring(i).startsWith(pat)) {
+                count++;
+            }
+        }
+        return count;
+        
+        /*
+        // 첫번재 방법
         int firstIdx = myString.indexOf(pat);
         
         if(firstIdx == -1)
@@ -15,5 +26,6 @@ class Solution {
         }
 
         return count;
+        */
     }
 }
