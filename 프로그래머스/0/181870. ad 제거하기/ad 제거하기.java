79a211ef49ddf47161ac665ce0f78c09281782a3
Jeong Jin Hui <jda0526@naver.com>
2025-03-22 20:35:18 +0900

    [level 0] Title: ad 제거하기, Time: 0.82 ms, Memory: 83.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] strArr) {
+        ArrayList<String> list = new ArrayList<>();
+        
+        for(String s : strArr){
+            if(!s.contains("ad"))
+                list.add(s);
+        }
+        
+        return list.toArray(new String[list.size()]);
+    }
+}
