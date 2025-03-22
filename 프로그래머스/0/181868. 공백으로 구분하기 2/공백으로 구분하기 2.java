e37dac561819bb9a9a686d62702949f982b56afa
Jeong Jin Hui <jda0526@naver.com>
2025-03-22 20:45:19 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 1.34 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String[] solution(String my_string) {
+        // 정규식 "\s"는 다음과 같은 종류의 공백(white space)을 나타낸다. : (\t, \n, \x0B, \f, \r)
+        // "+"는 1번이상을 의미한다. 즉, "\s+"는 1번 이상의 공백을 의미한다.
+        // trim : 앞, 뒤 공백 제거
+        return my_string.replaceAll("\\s+", " ").trim().split(" ");
+    }
+}
