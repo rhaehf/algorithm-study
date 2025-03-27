2f016b3decf926623524b1662170bb0d2b1d8da9
Jeong Jin Hui <jda0526@naver.com>
2025-03-27 21:43:56 +0900

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 31.42 ms, Memory: 94.9 MB -BaekjoonHub

--- 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
@@ -2,6 +2,11 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String myString) {
+        // 두번째 풀이
+        return Arrays.stream(myString.split("x")).filter(s -> !s.isEmpty()).sorted().toArray(String[]::new);
+        
+        /*
+        // 첫번째 풀이
         String[] answer = myString.split("x");
         // split 사용시 "xaxbxcx" 을 처리하면 ["","a","b","c"]이 나온다.
         
@@ -14,5 +19,6 @@ class Solution {
             
         Collections.sort(list);
         return list.toArray(String[]::new);
+        */
     }
 }
