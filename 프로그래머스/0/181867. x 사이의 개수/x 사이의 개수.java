de478495a317ddec35c2cb1bff6476da4c9bb1f6
Jeong Jin Hui <jda0526@naver.com>
2025-03-27 21:00:34 +0900

    [level 0] Title: x 사이의 개수, Time: 11.21 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.java
+++ 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.java
@@ -3,6 +3,19 @@ import java.util.*;
 class Solution {
     public int[] solution(String myString) {
         ArrayList<Integer> list = new ArrayList<>();
+        
+        // 두번째 풀이
+        String[] tmp = myString.split("x");
+        for (int i = 0; i < tmp.length; i++) {
+            list.add(tmp[i].length());
+        }
+
+        if (myString.endsWith("x")) {
+            list.add(0);
+        }
+        
+        /*
+        // 첫번째 풀이
         int count = 0;
         
         for (char ch : myString.toCharArray()) {
@@ -14,6 +27,7 @@ class Solution {
             }
         }
         list.add(count);
+        */
         
         return list.stream().mapToInt(i -> i).toArray();
     }
