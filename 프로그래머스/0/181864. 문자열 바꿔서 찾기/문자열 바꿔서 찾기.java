9c3c4e17ed626c86ec5616a163dd022d1ac7e3e8
Jeong Jin Hui <jda0526@naver.com>
2025-03-29 08:39:39 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.04 ms, Memory: 89.7 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -1,8 +1,13 @@
 class Solution {
     public int solution(String myString, String pat) {
+        // 한 번에 이어서 적어도 됨
+        myString = myString.replace("A", "a").replace("B", "A").replace("a", "B");
+        
+        /*
         myString = myString.replace("A", "C");
         myString = myString.replace("B", "A");
         myString = myString.replace("C", "B");
+        */
         
         return myString.contains(pat) ? 1 : 0;
     }
