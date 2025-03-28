f28dd6df545d91ff693fae9ea136c43d111b86b9
Jeong Jin Hui <jda0526@naver.com>
2025-03-28 19:38:46 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.12 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.java
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.java
@@ -2,6 +2,22 @@ import java.util.*;
 
 class Solution {
     public int solution(String binomial) {
+        // 두번째 방법
+        String[] parts = binomial.split(" ");
+        int a = Integer.parseInt(parts[0]);
+        int b = Integer.parseInt(parts[2]);
+        char op = parts[1].charAt(0);
+
+        if (op == '+') {
+            return a + b;
+        } else if (op == '-') {
+            return a - b;
+        } else {
+            return a * b;
+        }
+        
+        /*
+        // 첫번째 방법 - StringTokenizer 사용하기
         if (binomial.contains("+")) {
             StringTokenizer st = new StringTokenizer(binomial, " + ");
             int a = Integer.parseInt(st.nextToken());
@@ -18,5 +34,6 @@ class Solution {
             int b = Integer.parseInt(st.nextToken().trim());
             return a * b;
         }
+        */
     }
 }
