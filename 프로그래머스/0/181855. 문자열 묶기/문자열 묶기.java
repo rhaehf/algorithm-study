957679488609914635585b965bd76ca2bb7cc160
Jeong Jin Hui <jda0526@naver.com>
2025-04-11 23:16:31 +0900

    [level 0] Title: 문자열 묶기, Time: 5.79 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
@@ -2,6 +2,22 @@ import java.util.*;
 
 class Solution {
     public int solution(String[] strArr) {
+        // 두번째 풀이
+        int answer = 0;
+        int[] lengArr = new int[31];
+
+        for(int i=0; i<strArr.length; i++) {
+            lengArr[strArr[i].length()]++;
+        }
+
+        for(int i=0; i<=30; i++) {
+            answer = Math.max(answer, lengArr[i]);
+        }
+
+        return answer;
+        
+        /*
+        // 첫번째 풀이
         HashMap<Integer, Integer> map = new HashMap<>();
         
         for(String str : strArr){
@@ -11,5 +27,6 @@ class Solution {
         
          Integer maxValue = Collections.max(map.values());
         return maxValue;
+        */
     }
 }
