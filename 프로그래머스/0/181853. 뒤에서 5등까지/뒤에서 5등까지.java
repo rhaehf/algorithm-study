82ea980500169b7c51293af3a6cbc24d6c44e3fb
Jeong Jin Hui <jda0526@naver.com>
2025-04-11 23:32:02 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 0.51 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
@@ -4,11 +4,14 @@ class Solution {
     public int[] solution(int[] num_list) {
         Arrays.sort(num_list);
         
+        return Arrays.copyOfRange(num_list, 0, 5);
+        
+        /*
         int[] answer = new int[5];
         for(int i = 0; i < 5; i++) {
             answer[i] = num_list[i];
-        }
-        
+        }        
         return answer;
+        */
     }
 }
