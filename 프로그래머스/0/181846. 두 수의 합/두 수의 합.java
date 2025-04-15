bb5576eb399064a1828520014ab07cd7f8b80892
Jeong Jin Hui <jda0526@naver.com>
2025-04-15 14:35:09 +0900

    [level 0] Title: 두 수의 합, Time: 455.59 ms, Memory: 109 MB -BaekjoonHub

---
+++ 프로그래머스/0/181846. 두 수의 합/두 수의 합.java
@@ -0,0 +1,9 @@
+import java.math.BigInteger;
+
+class Solution {
+    public String solution(String a, String b) {
+        BigInteger bigNumber1 = new BigInteger(a);
+        BigInteger bigNumber2 = new BigInteger(b);
+        return bigNumber1.add(bigNumber2).toString();
+    }
+}
