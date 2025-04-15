df7141a34d701e7122150f846fceca3d4a8c6b4a
Jeong Jin Hui <jda0526@naver.com>
2025-04-15 14:39:26 +0900

    [level 0] Title: 문자열로 변환, Time: 6.43 ms, Memory: 79.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181845. 문자열로 변환/문자열로 변환.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(int n) {
+        return n + ""; // 방법 1
+        //return String.valueOf(n); // 방법 2
+        //return Integer.toString(n); // 방법 3
+    }
+}
