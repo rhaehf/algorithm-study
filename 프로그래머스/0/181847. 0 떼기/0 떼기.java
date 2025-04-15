9c3f6bd3eb0f7f43fa0dc241897ad4e45a546ddc
Jeong Jin Hui <jda0526@naver.com>
2025-04-15 14:29:37 +0900

    [level 0] Title: 0 떼기, Time: 7.86 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.java
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -1,5 +1,10 @@
 class Solution {
     public String solution(String n_str) {
+        // 두번째 방법 - 숫자로 변환하면서 앞에 0은 날라감
+        return "" + Integer.parseInt(n_str);
+        
+        /*
+        // 첫번째 방법
         String answer = "";
         
         for(char ch : n_str.toCharArray()){
@@ -9,5 +14,6 @@ class Solution {
                 answer += ch;            
         }
         return answer;
+        */
     }
 }
