5e6efb0457e4b36d334cabf8eeafc959acbd00a5
Jeong Jin Hui <jda0526@naver.com>
2025-04-17 21:22:51 +0900

    [level 2] Title: 의상, Time: 0.20 ms, Memory: 83.9 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -4,24 +4,25 @@ class Solution {
     public int solution(String[][] clothes) {
         HashMap<String, Integer> map = new HashMap<>();
         
+        // 옷 종류가 1개일 때
         if(map.keySet().size() == 1)
             return clothes.length;
         
-        // 종류별로 몇 개씩 있는지 저장 
+        // 종류별로 몇 개씩 있는지 저장
         for(String[] cloth : clothes) {
-            String key = cloth[1]; // 의상의 종류
-            map.put(key, map.getOrDefault(key, 0) + 1); // map(의상의 종류, 옷 개수)
+            String key = cloth[1]; // 옷 종류
+            map.put(key, map.getOrDefault(key, 0) + 1); // map(옷 종류, 옷 개수)
         }
         
         int answer = 1;
-        // 서로 독립적인 선택을 하는 경우, 경우의 수는 곱한다는 조합의 기본 원리
         for (String key : map.keySet()) {
+            // 서로 독립적인 선택을 하는 경우, 경우의 수는 곱한다는 조합의 기본 원리
+            answer *= (map.get(key) + 1); // key : 각 종류마다 옷 개수, +1 : 안 입는 경우
             // headgear : 총 3가지 (안입음, yellow_hat, green_turban)
             // eyewear: 총 2가지 (안입음, blue_sunglasses)
-            answer *= (map.get(key) + 1); // key : 각 종류마다 옷 개수, +1 : 안 입는 경우
-        }
-               
-        answer -= 1; // 모두 안 입는 경우 1개 제거         
+        }               
+        answer -= 1; // 모두 안 입는 경우 1개 제거
+        
         return answer;
     }
 }
