831802e79c95ab99e500078869f84e65f6c32103
Jeong Jin Hui <jda0526@naver.com>
2025-04-20 00:21:31 +0900

    [level 0] Title: 정수 찾기, Time: 2.79 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
@@ -1,8 +1,10 @@
 import java.util.*;
+import java.util.stream.IntStream;
 
 class Solution {
     public int solution(int[] num_list, int n) {
         // anyMatch : 최소 한 개의 요소가 조건에 만족하는지 true, false 반환
-        return Arrays.stream(num_list).anyMatch(x -> x == n) ? 1 : 0;
+        // return Arrays.stream(num_list).anyMatch(x -> x == n) ? 1 : 0;
+        return IntStream.of(num_list).anyMatch(num -> num == n) ? 1 : 0;
     }
 }
