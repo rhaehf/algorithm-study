b2dbc706fc9d6381de79991b0035b6010024eb81
Jeong Jin Hui <jda0526@naver.com>
2025-04-20 01:00:41 +0900

    [level 0] Title: 날짜 비교하기, Time: 1.08 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/README.md
+++ 프로그래머스/0/181838. 날짜 비교하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 날짜 비교하기 - 181838 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181838) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181838#) 
 
 ### 성능 요약
 
-메모리: 82.4 MB, 시간: 0.03 ms
+메모리: 88.6 MB, 시간: 1.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 20일 00:49:35
+2025년 04월 20일 01:00:39
 
 ### 문제 설명
 
--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
@@ -1,5 +1,13 @@
+import java.time.LocalDate;
+
 class Solution {
     public int solution(int[] date1, int[] date2) {
+        LocalDate dateA = LocalDate.of(date1[0], date1[1], date1[2]);
+        LocalDate dateB = LocalDate.of(date2[0], date2[1], date2[2]);
+
+        return dateA.isBefore(dateB) ? 1 : 0;
+            
+        /*
         if (date1[0] < date2[0]) 
             return 1;
         else if(date1[0] > date2[0])
@@ -16,5 +24,6 @@ class Solution {
                     return 0;
             }
         }
+        */
     }
 }
