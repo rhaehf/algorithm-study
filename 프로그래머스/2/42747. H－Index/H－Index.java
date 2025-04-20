15ca813d96a5d5a79e9b2c372a6feeebe08a1d59
Jeong Jin Hui <jda0526@naver.com>
2025-04-20 22:59:10 +0900

    [level 2] Title: H-Index, Time: 0.90 ms, Memory: 82.5 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -2,6 +2,20 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] citations) {
+        // 두 번째 풀이
+        Arrays.sort(citations);  // 오름차순 정렬
+
+        for (int i = 0; i < citations.length; i++) {
+            int h = citations.length - i; // 현재 인덱스보다 오른쪽에 있는 논문 개수
+
+            if (h <= citations[i]) { // h번 이상 인용된 논문이 h편 이상이면
+                return h;
+            }
+        }
+        return 0;
+        
+        /* 
+        // 첫 번째 풀이
         // int형 내림차순 정렬은 Integer로 가능해서 형변환 해야함
         Integer[] desc = Arrays.stream(citations).boxed().toArray(Integer[]::new);
         Arrays.sort(desc, Collections.reverseOrder());
@@ -19,5 +33,6 @@ class Solution {
             maxUse--;
         }
         return maxUse;
+        */
     }
 }
--- 프로그래머스/2/42747. H－Index/README.md
+++ 프로그래머스/2/42747. H－Index/README.md
@@ -1,10 +1,10 @@
 # [level 2] H-Index - 42747 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747#) 
 
 ### 성능 요약
 
-메모리: 92 MB, 시간: 22.30 ms
+메모리: 82.5 MB, 시간: 0.90 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 20일 22:02:44
+2025년 04월 20일 22:59:08
 
 ### 문제 설명
 
