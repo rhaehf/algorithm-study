c51ab2a5b6dfc913e5cbfdb95af1a8684d0c6ef8
Jeong Jin Hui <jda0526@naver.com>
2025-04-27 01:48:35 +0900

    [level 0] Title: 커피 심부름, Time: 0.74 ms, Memory: 87.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(String[] order) {
+        int cntAmericano = 0;
+        int cntCafelatte = 0;
+        
+        for (String menu : order) {
+            if (menu.contains("cafelatte"))
+                cntCafelatte++;                
+            else
+                cntAmericano++;
+        }
+        
+        return cntAmericano * 4500 + cntCafelatte * 5000;
+    }
+}
