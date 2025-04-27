98a1735589d929f21ab15f25f5422410de658cf0
Jeong Jin Hui <jda0526@naver.com>
2025-04-27 21:36:57 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 0.14 ms, Memory: 90 MB -BaekjoonHub

--- 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.java
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.java
@@ -1,5 +1,19 @@
 class Solution {
-    public int solution(int[][] arr) {       
+    public int solution(int[][] arr) {   
+        // 두번째 방법 - 삼각형 절반(상삼각형)만 확인함
+        // 상삼각형 : 2차원 배열에서 행 번호 ≤ 열 번호(i ≤ j)인 위치만을 보는 것
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) { // j = i+1부터 검사
+                if (arr[i][j] != arr[j][i]) {
+                    return 0;
+                }
+            }
+        }
+        // 여전히 O(N²)지만 이전과 비교해서 검사 횟수가 절반이다.
+        return 1;
+        
+        /*
+        // 첫번째 방법 - 전체 대칭 행렬 검사
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length; j++) {
                 if(arr[i][j] == arr[j][i])
@@ -9,5 +23,6 @@ class Solution {
             }
         }
         return 1;
+        */
     }
 }
