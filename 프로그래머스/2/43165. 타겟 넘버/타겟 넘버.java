3fed608107bf5c825aa4b4e1d66276e2346a358a
Jeong Jin Hui <jda0526@naver.com>
2025-05-09 20:27:29 +0900

    [level 2] Title: 타겟 넘버, Time: 4.38 ms, Memory: 85.7 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,4 +1,25 @@
-// DFS 풀이
+// DFS 풀이 2 -> 리턴값 누적 방식
+class Solution {    
+    public int solution(int[] numbers, int target) {
+        // 깊이, 숫자 배열, 목표 숫자, 현재 합계
+        return dfs(0, numbers, target, 0); // 재귀 함수 호출
+    }
+
+    private int dfs(int depth, int[] numbers, int target, int sum) {
+        if (depth == numbers.length) { // 배열의 모든 숫자를 사용했을 때
+            if (sum == target) { // 목표 숫자와 현재 합계가 같으면
+                return 1; // 유효한 조합이므로 1가지 방법을 찾아서 1을 반환
+            }
+            return 0; // 합계가 맞지 않으면 실패한 경로이므로 0을 반환
+        }
+        // 모든 경로에서 성공한 경우를 1씩 반환하고, 재귀적으로 + 연산하여 총 합을 리턴
+        return dfs(depth + 1, numbers, target, sum + numbers[depth])
+             + dfs(depth + 1, numbers, target, sum - numbers[depth]);
+    }
+}
+
+// DFS 풀이 1 -> 전역 변수 방식
+/*
 class Solution {  
     int count = 0;
 
@@ -9,9 +30,9 @@ class Solution {
     }
 
     private void dfs(int depth, int[] numbers, int target, int sum) {
-        if (depth == numbers.length) { // 배열의 숫자를 모두 사용했을 때
-            if (sum == target) { // 목표 숫자와 현재 함계가 같으면
-                count++; // 케이스를 하나 찾은 것이다.
+        if (depth == numbers.length) { // 배열의 모든 숫자를 사용했을 때
+            if (sum == target) { // 목표 숫자와 현재 합계가 같으면
+                count++; // 유효한 조합이므로 1가지 방법을 찾았다.
             }
             return;
         }
@@ -20,3 +41,4 @@ class Solution {
         dfs(depth + 1, numbers, target, sum - numbers[depth]);
     }
 }
+*/
