c2237815bdf123b2c026895224eb274d9f23b460
Jeong Jin Hui <jda0526@naver.com>
2025-05-15 22:06:13 +0900

    [level 3] Title: 입국심사, Time: 108.57 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/README.md
+++ 프로그래머스/3/43238. 입국심사/README.md
@@ -1,10 +1,10 @@
 # [level 3] 입국심사 - 43238 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238) 
 
 ### 성능 요약
 
-메모리: 85.1 MB, 시간: 110.14 ms
+메모리: 101 MB, 시간: 108.57 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 15일 20:56:29
+2025년 05월 15일 22:06:12
 
 ### 문제 설명
 
--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -2,26 +2,27 @@ import java.util.*;
 
 class Solution {
     public long solution(int n, int[] times) {
-        Arrays.sort(times);
         long answer = 0;
-        long left = (long)times[0];
-        long right = (long)times[times.length-1] * n;
+        Arrays.sort(times);        
+        long minTime = (long)times[0]; // 가능한 최소 시간 (가장 빠른 심사관이 1명 심사하는 시간)
+        long maxTime = (long)times[times.length-1] * n; // 가능한 최대 시간 (가장 느린 심사관이 모든 사람을 다 심사하는 최악의 경우)
         
-        while (left <= right) {
-            long mid = (left + right) / 2; // 중간 시간
-            long peopleProcessed = 0;
+        while (minTime <= maxTime) {
+            long mid = (minTime + maxTime) / 2; // 이분 탐색의 중간 시간
+            long pass = 0; // 입국심사를 통과한 인원
 
-            // mid 시간 동안 처리 가능한 총 인원 계산
+            // mid 시간 동안 처리 가능한 인원 계산
             for (int time : times) {
-                peopleProcessed += mid / time;
-                if (peopleProcessed >= n) break; // n명을 초과하면 더 계산할 필요 없음
+                pass += mid / time; // 해당 시간 내에 몇 명을 심사 가능한지 누적 계산
+                if (pass >= n) 
+                    break; // 통과 인원이 n명을 초과하면 더 계산할 필요 없음
             }
 
-            if (peopleProcessed >= n) {
-                answer = mid; // 더 작은 시간을 탐색
-                right = mid - 1;
-            } else {
-                left = mid + 1; // 시간을 더 늘림
+            if (pass >= n) { // 통과 가능한 인원이 통과를 기다리는 사람보다 많아지면
+                answer = mid; // 가능한 시간 저장
+                maxTime = mid - 1; // 더 작은 시간을 탐색하기 위해 최대 시간 범위를 줄임
+            } else { // 통과 가능한 인원이 부족하므로
+                minTime = mid + 1; // 시간 부족이기에 최소 시간 범위를 늘림
             }
         }
 
