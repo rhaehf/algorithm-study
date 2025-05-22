bfb20607f5246372d8bf422b04202e987d965813
Jeong Jin Hui <jda0526@naver.com>
2025-05-22 18:55:39 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.70 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -1,29 +1,19 @@
 import java.util.*;
 
 class Solution {
-    public String[] solution(String[] strings, int n) {        
-        List<String> list = new ArrayList<>(Arrays.asList(strings));
-        
-        // 문자열의 n번째 인덱스 기준으로 정렬
-        list.sort(new Comparator<String>() {
+    public String[] solution(String[] strings, int n) {
+        Arrays.sort(strings, new Comparator<String>() {
             @Override
             public int compare(String a, String b) {
-                return a.charAt(n) - b.charAt(n);
+                // 1. n번째 문자가 다르면 그 문자 기준 정렬
+                if (a.charAt(n) != b.charAt(n)) {
+                    return Character.compare(a.charAt(n), b.charAt(n));
+                }
+                // 2. n번째 문자가 같으면 사전순 정렬
+                return a.compareTo(b);
             }
-        });        
-        //System.out.println(list);
-        
-        // n번째 인덱스가 같으면 사전순으로 다시 정렬
-        list.sort(new Comparator<String>() {
-            @Override
-            public int compare(String a, String b) {
-                if(a.charAt(n) == b.charAt(n))
-                    return a.compareTo(b);
-                else return 0;
-            }
-        });        
-        //System.out.println(list);
-        
-        return list.toArray(new String[0]);
+        });
+
+        return strings;
     }
 }
