2c809147657de3c2aab7b8e9f06ee6b4ee7e097c
Jeong Jin Hui <jda0526@naver.com>
2025-06-19 21:09:30 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.09 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,11 +1,10 @@
 class Solution {
     public int solution(String s) {
-        String[] alpha = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-        String[] num = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+        String[] alphabets = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
         
         for (int i = 0; i < 10; i++) {
-            if (s.contains(alpha[i])) {
-                s = s.replace(alpha[i], num[i]);
+            if (s.contains(alphabets[i])) {
+                s = s.replace(alphabets[i], Integer.toString(i));
                 //System.out.println(s);
             }            
         }
