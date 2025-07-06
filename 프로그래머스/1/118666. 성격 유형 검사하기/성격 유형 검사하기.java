12bff499cb23a7d20e0dbfef694224785066729d
Jeong Jin Hui <jda0526@naver.com>
2025-07-07 00:58:06 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.73 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -2,62 +2,40 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] survey, int[] choices) {
-        HashMap<String, Integer> map = new HashMap<>();
-        
-        map.put("R", 0); // 라이언형
-        map.put("T", 0); // 튜브형
-        map.put("C", 0); // 콘형
-        map.put("F", 0); // 프로도형
-        map.put("J", 0); // 제이지형
-        map.put("M", 0); // 무지형
-        map.put("A", 0); // 어피치형
-        map.put("N", 0); // 네오형
-        
-        for (int i = 0; i < survey.length; i++) {            
-            String type1 = String.valueOf(survey[i].charAt(0)); // 질문 타입
-            String type2 = String.valueOf(survey[i].charAt(1));
+        // 성격 유형 점수를 저장할 Map
+        Map<Character, Integer> map = new HashMap<>();
+        for (char type : new char[] {'R', 'T', 'C', 'F', 'J', 'M', 'A', 'N'}) {
+            map.put(type, 0);
+        }
+
+        // 점수 계산
+        for (int i = 0; i < survey.length; i++) {
+            char type1 = survey[i].charAt(0); // 질문 타입
+            char type2 = survey[i].charAt(1);
             int choice = choices[i]; // 선택지 점수
-            
-            // 모르겠음 선택지를 선택하면 점수를 얻지 않는다.
-            if (choice == 4) continue;
-            
-            if (choice <= 3) {
-                map.put(type1, map.getOrDefault(type1, 0) + (4 - choice));                          
-            } else if (5 <= choice) {
-                map.put(type2, map.getOrDefault(type2, 0) + (choice - 4));                
+
+            // 모르겠음(choice 4번) 선택지를 선택하면 점수를 얻지 않는다.
+            if (choice < 4) {
+                map.put(type1, map.get(type1) + (4 - choice));
+            } else if (choice > 4) {
+                map.put(type2, map.get(type2) + (choice - 4));
             }
         }
-        
-        /*for (String key : map.keySet()) {
-            System.out.println(key + " = " + map.get(key));
-        }*/
-        
-        StringBuilder sb = new StringBuilder();
-        
-        // 1번 지표
-        if (map.get("R") < map.get("T"))
-            sb.append("T");
-        else
-            sb.append("R");
-        
-        // 2번 지표
-        if (map.get("C") < map.get("F"))
-            sb.append("F");
-        else
-            sb.append("C");
-        
-        // 3번 지표
-        if (map.get("J") < map.get("M"))
-            sb.append("M");
-        else
-            sb.append("J");
-        
-        // 4번 지표
-        if (map.get("A") < map.get("N"))
-            sb.append("N");
-        else
-            sb.append("A");
-        
-        return sb.toString();
+
+        // 성격 유형 결과 조합
+        StringBuilder result = new StringBuilder();
+        char[][] indicators = {{'R', 'T'}, {'C', 'F'}, {'J', 'M'}, {'A', 'N'}};
+
+        for (char[] pair : indicators) {
+            char first = pair[0];
+            char second = pair[1];
+            if (map.get(first) < map.get(second)) {
+                result.append(second);
+            } else {
+                result.append(first); // 점수가 같으면 사전 순으로 빠른 앞 순서 우선
+            }
+        }
+
+        return result.toString();
     }
 }
