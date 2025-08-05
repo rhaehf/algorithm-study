a77815657f9fa93415e6f5feca0076a996b02cef
Jeong Jin Hui <jda0526@naver.com>
2025-08-05 20:12:45 +0900

    [level 1] Title: 카드 뭉치, Time: 0.03 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -2,23 +2,16 @@ class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
         int idx1 = 0; // 카드 뭉치1 인덱스
         int idx2 = 0; // 카드 뭉치2 인덱스
-        
+
         for (String word : goal) {
-            boolean flag = false; // 목표 단어가 있는지
-            
-            // 카드뭉치에서 목표 단어(word) 찾기
+		        // 카드뭉치에서 목표 단어(word) 찾기
             if (idx1 < cards1.length && cards1[idx1].equals(word)) {
-                flag = true;
                 idx1++;
-            }                
-            else if (idx2 < cards2.length && cards2[idx2].equals(word)) {
-                flag = true;
+            } else if (idx2 < cards2.length && cards2[idx2].equals(word)) {
                 idx2++;
+            } else {
+                return "No";
             }
-            
-            // 목표 단어를 찾지 못하면 바로 "No" 리턴
-            if (!flag)
-                return "No";         
         }
 
         return "Yes";
