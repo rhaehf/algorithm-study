4a575fc40ba1aef4f53bc6c5797b13763cd2644f
Jeong Jin Hui <jda0526@naver.com>
2025-09-23 00:45:38 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.47 ms, Memory: 85.1 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -2,29 +2,35 @@ import java.util.Stack;
 
 class Solution {
     public int solution(int[][] board, int[] moves) {
-        int answer = 0;
-        int size = board.length; // 격자 크기
+        // 기계의 열에 대한 스택 배열 생성
+        Stack<Integer>[] lanes = new Stack[board.length];
+        for (int i = 0; i < lanes.length; i++)
+            lanes[i] = new Stack<>();
+        
+        // 역순으로 탐색하면서 기계의 인형을 스택에 추가
+        for (int i = board.length - 1; i >= 0; i--) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] > 0)
+                    lanes[j].push(board[i][j]);
+            }
+        }
+        
+        int answer = 0; // 사라진 인형 세기
         Stack<Integer> stack = new Stack<>(); // 인형 담는 바구니
         
         for (int move : moves) {
-            int y = move - 1;
-            
-            for (int row = 0; row < size; row++) {
-                int pick = board[row][y];
-                if (pick != 0) {
-                    // 집어 올린 인형과 이전에 있던 인형이 같으면
-                    if (!stack.isEmpty() && pick == stack.peek()) {
-                        stack.pop();
-                        answer += 2;                        
-                    }
-                    // 인형이 같지 않으면 바구니에 넣기
-                    else
-                        stack.push(pick);
-                    
-                    //System.out.println(move + " : " + row + ", " + y + " - " + stack);                     
-                    board[row][y] = 0; // 집어 올린 인형 자리는 빈 상태로 바꾸기
-                    break; // 인형을 집었으니까 다음 move로 넘어가기
+            // 해당 열에 인형이 있으면
+            if (!lanes[move - 1].isEmpty()) {
+                int doll = lanes[move - 1].pop();
+                
+                // 집어 올린 인형과 가장 위에 있는 인형이 같으면
+                if (!stack.isEmpty() && doll == stack.peek()) {
+                    stack.pop();
+                    answer += 2;                        
                 }
+                // 인형이 같지 않으면 바구니에 넣기
+                else
+                    stack.push(doll);
             }
         }
         
