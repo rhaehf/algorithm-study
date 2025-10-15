0d309ba32fd37d985c3f9cd2bafcd8a3aa0756d9
Jeong Jin Hui <jda0526@naver.com>
2025-10-16 00:11:22 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 38.36 ms, Memory: 93.4 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
@@ -1,7 +1,7 @@
 class Solution {
     static int EMOTICON_PLUS; // 총 이모티콘 플러스 서비스 가입자수
     static int TOTAL_SALES; // 총 이모티콘 구매 비용
-    static final int[] SALE_RATE = {10, 20, 30, 40}; // 이모티콘 할인률
+    static final int[] SALE_RATE = {10, 20, 30, 40}; // 이모티콘 할인율
     
     public int[] solution(int[][] users, int[] emoticons) {
         EMOTICON_PLUS = 0; // 매 테스트 케이스 호출마다 초기화
@@ -11,11 +11,11 @@ class Solution {
         return new int[] {EMOTICON_PLUS, TOTAL_SALES};
     }
     
-    // 재귀 기반 DFS 방식으로 완전 탐색을 수행해 이모티콘 할인률 조합 생성
+    // 재귀 기반 DFS 방식으로 완전 탐색을 수행해 이모티콘 할인율 조합 생성
     private void setRateRecur(int[][] users, int[] emoticons, int index, int[] rates) {
-        // index: 몇 번째 이모티콘에 접근했는지, rates: saleRate 중 이모티콘에 적용할 할인률 담는 배열
+        // index: 몇 번째 이모티콘에 접근했는지, rates: saleRate 중 이모티콘에 적용할 할인율 담는 배열
         
-        // 제일 마지막 이모티콘까지 재귀 호출되었다면, 최적의 할인률 조합인지 확인
+        // 제일 마지막 이모티콘까지 재귀 호출되었다면, 최적의 할인율 조합인지 확인
         if (index == emoticons.length) {
             findComb(users, emoticons, rates);
             return;
@@ -23,23 +23,23 @@ class Solution {
         
         for (int rate : SALE_RATE) {
             rates[index] = rate;
-            // 다음 이모티콘의 할인률을 정하러 재귀함수 사용
+            // 다음 이모티콘의 할인율을 정하러 재귀함수 사용
             setRateRecur(users, emoticons, index + 1, rates);
         }
     }
     
-    // 최적의 이모티콘 할인률 조합인지 확인
+    // 최적의 이모티콘 할인율 조합인지 확인
     private void findComb(int[][] users, int[] emoticons, int[] rates) {
-        int emoticonPlus = 0; // 현재 할인률 조합의 이모티콘 플러스 가입자수
-        int totalSales = 0; // 현재 할인률 조합의 이모티콘 구매 비용
+        int emoticonPlus = 0; // 현재 할인율 조합의 이모티콘 플러스 가입자수
+        int totalSales = 0; // 현재 할인율 조합의 이모티콘 구매 비용
         
         for (int[] user : users) {
             int purchase = 0; // 이모티콘 구매 비용
-            int rate = user[0]; // 해당 할인률 이상이어야 사용자가 이모티콘을 구매함
+            int rate = user[0]; // 해당 할인율 이상이어야 사용자가 이모티콘을 구매함
             int maxPurchase = user[1]; // 사용자가 지불 가능한 최대 이모티콘 구매 비용
             
             for (int i = 0; i < rates.length; i++) {
-                // 이모티콘 할인률이 더 크다면 이모티콘을 구매함
+                // 이모티콘 할인율이 더 크다면 이모티콘을 구매함
                 if (rate <= rates[i]) {
                     purchase += (emoticons[i] * (100 - rates[i]) / 100);
                 }
@@ -51,7 +51,7 @@ class Solution {
             else totalSales += purchase;
         }
         
-        // 현재 할인률 조합의 이모티콘 플러스 가입자수가 많다면 총 결과 업데이트
+        // 현재 할인율 조합의 이모티콘 플러스 가입자수가 많다면 총 결과 업데이트
         if (EMOTICON_PLUS < emoticonPlus) {
             EMOTICON_PLUS = emoticonPlus;
             TOTAL_SALES = totalSales;
