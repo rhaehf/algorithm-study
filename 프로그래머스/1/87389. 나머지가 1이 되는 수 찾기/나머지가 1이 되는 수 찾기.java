54efa5124551c11224744551add58fb9968c2a12
Jeong Jin Hui <jda0526@naver.com>
2025-10-30 21:11:36 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 7.78 ms, Memory: 85.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 2;        
+        while (answer < n) {
+            if (n % answer == 1)
+                break;
+            else answer++;
+        }
+        return answer;
+    }
+}
