7acc634197c6ad866b4073d42331b8a42b3b6632
Jeong Jin Hui <jda0526@naver.com>
2025-12-19 20:15:51 +0900

    [level 1] Title: 약수의 합, Time: 0.07 ms, Memory: 90.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n) {
+        if (n == 0) return 0;
+        
+        int answer = 1;
+        for (int i = 2; i <= n; i++) {
+            if (n % i == 0) answer += i;
+        }
+        return answer;
+    }
+}
