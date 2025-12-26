1769ec6081fb415eab06417953afc9f97ace6488
Jeong Jin Hui <jda0526@naver.com>
2025-12-26 19:26:10 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 12.26 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String str1, String str2) {
+        Map<String, Integer> map1 = makeSet(str1);
+        Map<String, Integer> map2 = makeSet(str2);
+        
+        // 집합이 모두 공집합이면 자카드 유사도는 1로 정의한다.
+        if (map1.isEmpty() && map2.isEmpty()) return 65536;
+        
+        int intersection = 0; // 교집합        
+        // 교집합 구하기
+        for (String key1 : map1.keySet()) {
+            // 같은 값을 여러개 가질 때
+            if (map2.containsKey(key1))
+                intersection += Math.min(map1.get(key1), map2.get(key1));
+        }
+        
+        int union = 0; // 합집합
+        Set<String> allKeys = new HashSet<>();
+        allKeys.addAll(map1.keySet());
+        allKeys.addAll(map2.keySet());
+        
+        // 합집합 구하기
+        for (String key : allKeys) {
+            union += Math.max(map1.getOrDefault(key, 0), map2.getOrDefault(key, 0));
+        }
+        
+        double result = (double) intersection / union * 65536;
+        return (int) result;
+    }
+    
+    // 다중집합 만들기    
+    private static Map<String, Integer> makeSet(String str) {
+        Map<String, Integer> multiset = new HashMap<>();
+        str = str.toLowerCase(); // 3. 소문자로 바꾸기
+
+        for (int i = 0; i < str.length() - 1; i++) {
+            // 1. 문자 2개씩 자르기 
+            char a = str.charAt(i); 
+            char b = str.charAt(i + 1);
+            
+            // 2. 영문자만 있는지 확인하기
+            if (a >= 'a' && a <= 'z' && b >= 'a' && b <= 'z') {
+                String s = "" + a + b;
+                multiset.put(s, multiset.getOrDefault(s, 0) + 1);
+            }
+        }
+
+        return multiset;
+    }
+}
